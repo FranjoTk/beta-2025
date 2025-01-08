5e8c2f232072705cef0019bfcf943a183f9244e4
FranjoTk <147728115+FranjoTk@users.noreply.github.com>
2025-01-08 20:58:05 +0100

    Update main_runtime.py

--- main_runtime.py
+++ main_runtime.py
@@ -8,7 +8,7 @@ from decorators import my_logger, my_timer
 import sys
 
 # Ausgabe in die Datei umleiten
-sys.stdout = open('/content/ausgabe2.txt', 'w')
+sys.stdout = open('/content/ausgabe.txt', 'w')
 
 # Load dataset
 data = pd.read_csv('Advertising.csv')
